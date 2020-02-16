0621b1cc91a711b732dd1bc5016a86c383e0c13d
Deus Matei <deus.matei@gmail.com>
2020-02-16 23:09:22 +0200

    Implemented file structure

---
+++ src/views/Login.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+       <v-content>
+  <LoginComponent/>
+       </v-content>
+       </div>
+</template>
+
+<script>
+import LoginComponent from '../components/LoginComponent.vue'
+
+export default {
+     components: {
+    LoginComponent
+  },
+}
+</script>

7632998ab82feea286d597098aefdd4f0c23d472
Deus Matei <deus.matei@gmail.com>
2020-02-17 23:21:48 +0200

    implemented nav guard and user status watch

--- src/router.js
+++ src/router.js
@@ -4,12 +4,23 @@ import Router from "vue-router";
 //Views
 import LoginView from "./views/Login.vue"
 import Weather from "./views/Weather.vue"
+import store from "./store";
 
 Vue.use(Router);
 
+const isSignedIn = (to, from, next) =>{
+    if(store.state.user) next();
+    else next("/")
+}
+
+const isSignedOut = (to, from, next) =>
+{
+    if(!store.state.user) next();
+    else next("/weather");
+}
 export default new Router({
     routes: [
-        {path: "/", component: LoginView},
-        {path: "/weather", component: Weather}
+        {path: "/", component: LoginView, beforeEnter: isSignedOut},
+        {path: "/weather", component: Weather, beforeEnter: isSignedIn}
     ]
 })

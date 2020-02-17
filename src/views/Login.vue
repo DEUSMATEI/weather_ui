2739ad1b0e9ab4eb7cd393bc9d8a948b0e92a24c
Deus Matei <deus.matei@gmail.com>
2020-02-17 20:34:00 +0200

    Implemented login view

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,17 +1,74 @@
 <template>
-    <div>
-       <v-content>
-  <LoginComponent/>
-       </v-content>
+    <div class="mainContainer">
+       <v-card flat>
+            <v-card-text>
+                 <v-text-field                    
+                    v-model = "email"
+                    placeholder="E-mail..."  
+                    :error-messages="emailErr"                  
+                    />
+                <v-text-field                    
+                    v-model = "password"
+                    placeholder="Password..."                                        
+                    type="password"
+                    :error-messages="passErr"
+                    />
+                    <v-btn
+                    @click="signIn"
+                    color="primary"
+                    block>Sign In</v-btn>
+            </v-card-text>
+       </v-card>
        </div>
 </template>
 
 <script>
-import LoginComponent from '../components/LoginComponent.vue'
 
 export default {
-     components: {
-    LoginComponent
-  },
+     data(){
+          return{
+               email: "",
+               emailErr: [],               
+               password: "",               
+               passErr: []
+          };
+     },
+     methods: {
+          signIn(){
+               /*call signIn method from store */
+          },
+          onSuccess(){
+               /*Router push to second view*/
+          },
+          onError(err){
+               const code = err.code;
+               switch(code){
+                    case "auth/invalid-email":
+                         this.emailErr = ["Wrong Email address"]
+                         break;
+                    case "auth/user-not-found":
+                         this.emailErr = ["User not found"]
+                         break;
+                    case "auth/wrong-password":
+                         this.passErr = ["Wrong password"]
+                         break;
+                    default:
+                         break;                         
+               }
+          }
+     }
+     
+
 }
 </script>
+
+<style scoped>
+.mainContainer{
+     height: 100%;
+     padding: 1.5rem;
+     display: flex;
+     flex-direction: column;
+     align-items: center;
+     justify-content: center;
+}
+</style>
